77114e4249eaefd3f06131032bfa4ec0f237b6eb
ask53 <askrupp@gmail.com>
2017-10-02 10:31:08 -0500

    change view reports text

--- display_Spanish.js
+++ display_Spanish.js
@@ -16,8 +16,8 @@ var CONTAMINANTS = ["Fluoruro", "Arsénico", "Nivel de peligro total"];
 											//	(since FLUORIDE == 0 above...)
 var DATE = "Fecha";
 var TEST_ORG = "Institución";
-var SEE_MORE = "Resultados completos";						// This message gets displayed as a link to show 
-															//	more info about the given datapoint
+var SEE_MORE = "Reporte(s) Oficial(es)";	// This message gets displayed as a link to show 
+											//	more info about the given datapoint
 var CARTO_ATTRIBUTION = 'Data hosting: <a href="http://www.dropbox.com">Dropbox</a> legends by <a href="http://www.carto.com">CartoDB</a>';
 
 var NO_DATA_MSG = "No hay datos";
